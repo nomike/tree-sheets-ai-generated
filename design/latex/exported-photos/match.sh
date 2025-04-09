c683a2d40d0f59c1dbbb15b442b715a010910a80
nomike <nomike@nomike.com>
2025-04-09 22:27:22 +0200

    Add exported photos

---
+++ design/latex/exported-photos/match.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+target_directory=../photos/
+source_file_extension=".png"
+target_file_extension=".eps"
+
+for file in *"${source_file_extension}"; do
+    base_name="${file%$source_file_extension}"
+    target_file="$(find ../photos/ -maxdepth 2 -type d -iname "${base_name}")"
+    if [ -d "$target_file" ]; then
+        target_file="${target_file}/${base_name}${target_file_extension}"
+        echo "Converting $file to $target_file"
+        convert "$file" "$target_file"
+    else
+        echo "No matching directory found for $file"
+    fi
+done
