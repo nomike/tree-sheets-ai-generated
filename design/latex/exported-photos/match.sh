7596ef63fadb2d3777cada508a0012ad55144b74
nomike <nomike@nomike.com>
2025-04-10 02:25:15 +0200

    Correct spelling

--- design/latex/exported-photos/match.sh
+++ design/latex/exported-photos/match.sh
@@ -2,16 +2,16 @@
 
 target_directory=../photos/
 source_file_extension=".png"
-target_file_extension=".eps"
+target_file_extension=".jpg"
 
 for file in *"${source_file_extension}"; do
     base_name="${file%$source_file_extension}"
     target_file="$(find ../photos/ -maxdepth 2 -type d -iname "${base_name}")"
     if [ -d "$target_file" ]; then
         target_file="${target_file}/${base_name}${target_file_extension}"
-        echo "Converting $file to $target_file"
+        # echo "Converting $file to $target_file"
         convert "$file" "$target_file"
     else
-        echo "No matching directory found for $file"
+        echo "${file}"
     fi
 done
